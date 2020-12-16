39c9e0f81785831beeab2762013760477a98088b
MrFranQx <franek.2005@interia.pl>
2020-12-16 18:00:12 +0100

    Moved "sh" to "bin" directory

--- boot.py
+++ boot.py
@@ -1,7 +1,7 @@
-import sh # import shell
+import bin.sh # import shell from bin directory
 
 def boot(): # define boot function
     try:
-        sh.sh() # run shell
+        bin.sh.sh() # run shell
     except EOFError: # if EOF raised, proceed with boot
         pass
